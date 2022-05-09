703f2a041f3a9f56f90da5fb539e5df97b50d87b
Demetrio Pacheco <61946635+Demetriodeis@users.noreply.github.com>
2022-05-09 14:46:11 -0300

    initial project

---
+++ GameChallenge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
